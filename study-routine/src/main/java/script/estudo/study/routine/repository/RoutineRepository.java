1d2d651c3e9b605a683f2c17f2c737627d75988e
Breno Chaves <103227487+Brenoch@users.noreply.github.com>
2025-04-21 15:02:44 -0300

    Delete study-routine directory

--- study-routine/src/main/java/script/estudo/study/routine/repository/RoutineRepository.java
+++
@@ -1,9 +0,0 @@
-package script.estudo.study.routine.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import script.estudo.study.routine.model.Routine;
-
-public interface RoutineRepository extends JpaRepository<Routine, Long > {
-
-}
