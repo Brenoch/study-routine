e36e913a465e4699a4c5ea06b8e369481c92a204
Breno Chaves <103227487+Brenoch@users.noreply.github.com>
2025-04-21 15:02:11 -0300

    Add files via upload

---
+++ study-routine/src/main/java/script/estudo/study/routine/StudyRoutineApplication.java
@@ -0,0 +1,13 @@
+package script.estudo.study.routine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRoutineApplication.class, args);
+	}
+
+}
---
+++ study-routine/src/test/java/script/estudo/study/routine/StudyRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package script.estudo.study.routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
